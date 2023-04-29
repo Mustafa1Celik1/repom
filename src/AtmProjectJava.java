5fb6caf321ec46a10d9acb4520f38d1b12176f60
Mustafa Celik <mustafacelik48484858@gmail.com>
2023-04-30 00:20:04 +0300

    v2

--- src/AtmProjectJava.java
+++ src/AtmProjectJava.java
@@ -2,5 +2,7 @@ public class AtmProjectJava {
     public static void main(String[] args) {
         System.out.println("bu bir atm projesidir");
         //version1
+        System.out.println("ggh");
+        //version2
     }
 }
